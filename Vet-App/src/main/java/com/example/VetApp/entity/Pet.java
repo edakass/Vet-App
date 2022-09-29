51ae248c829090c32750438287250b4381b9e957
Eda Kaş <eda.kas60@gmail.com>
2022-09-29 13:53:06 +0300

    Entity'ler oluşturuldu

---
+++ Vet-App/src/main/java/com/example/VetApp/entity/Pet.java
@@ -0,0 +1,87 @@
+package com.example.VetApp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="pet")
+public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+    @Column(name="petType")
+    private String petType;
+
+    private String petBreed; //cinsi
+
+    private String petName;
+
+    private int petAge;
+
+    private  String petDescription;
+
+    public  Pet(){
+
+    }
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+
+    public Pet(Long id, String petType, String petBreed, String petName, int petAge, String petDescription, User user) {
+        this.id = id;
+        this.petType = petType;
+        this.petBreed = petBreed;
+        this.petName = petName;
+        this.petAge = petAge;
+        this.petDescription = petDescription;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPetType() {
+        return petType;
+    }
+
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+    public String getPetBreed() {
+        return petBreed;
+    }
+
+    public void setPetBreed(String petBreed) {
+        this.petBreed = petBreed;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public int getPetAge() {
+        return petAge;
+    }
+
+    public void setPetAge(int petAge) {
+        this.petAge = petAge;
+    }
+
+    public String getPetDescription() {
+        return petDescription;
+    }
+
+    public void setPetDescription(String petDescription) {
+        this.petDescription = petDescription;
+    }
+}
--- Vet-App/src/main/java/com/example/VetApp/entity/User.java
+++ Vet-App/src/main/java/com/example/VetApp/entity/User.java
@@ -1,6 +1,8 @@
 package com.example.VetApp.entity;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
 
 @Entity
 @Table(name="user",uniqueConstraints = @UniqueConstraint(columnNames = "email" ))
@@ -10,12 +12,99 @@ public class User {
     private Long id;
 
     @Column(name="first_name")
-    private String firstName;
+    private String fullName;
 
     private String email;
 
     private String password;
 
-    //fist deneme
-    //oldu mu
+    private String telNumber;
+
+    private String contactInformation;
+    // iletişim bilgileri, telefon,
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(
+                    name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "role_id", referencedColumnName = "id"))
+
+    private Collection<Role> roles;
+
+    @OneToMany(mappedBy="user")
+    private Set<Pet> pets;
+
+
+    public  User (){ //boş constructor oluşturuldu çünkü daha sonraki classlarla iletişim sağlamak için
+
+    }
+
+    public User(Long id, String fullName, String email, String password, String telNumber, String contactInformation, Collection<Role> roles) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.telNumber = telNumber;
+        this.contactInformation = contactInformation;
+        this.roles = roles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getContactInformation() {
+        return contactInformation;
+    }
+
+    public void setContactInformation(String contactInformation) {
+        this.contactInformation = contactInformation;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
 }
